b6c672f1611b5072f298585e2140c97c54d3de4f
ajshopov <alexanderjshopov@gmail.com>
2019-07-23 22:23:03 +1000

    create react app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const App = (props) => {
+// console.log(useState(0)) // returns [`current state value`, `function to update state`]
+  const [count, setCount] = useState(props.count)
+
+  return(
+    <div>
+      <p>this is the count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(props.count)}>reset</button>
+    </div>
+  )
+}
+
+App.defaultProps = {
+  count: 10
+}
+
+ReactDOM.render(<App count={5} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
